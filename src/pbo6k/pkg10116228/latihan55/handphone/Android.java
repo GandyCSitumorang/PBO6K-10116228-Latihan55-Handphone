5084373e3ea432b56b9f81ed2a28ea35ba1e510f
MY PC <MY PC@HOME-UEFI1TB.local>
2019-01-16 03:54:16 +0700

    Project GandyCSitumorang 10116228 PBO6K

---
+++ src/pbo6k/pkg10116228/latihan55/handphone/Android.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116228.latihan55.handphone;
+
+/**
+ *
+ * @author MY PC
+ */
+public class Android extends handphone{
+    private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+}
---
+++ src/pbo6k/pkg10116228/latihan55/handphone/PBO6K10116228Latihan55Handphone.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116228.latihan55.handphone;
+
+/**
+ *Nama  : GandyChristianSitumorang
+ *Kelas : PBO6K
+ *NIM   : 10116228
+ *Deskripsi Program : Program ini berisi program yang menampilkan manufaktur
+ *                    dari sebuah handphone
+ */
+public class PBO6K10116228Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Android adr = new Android("Samsung","Android","J5",3000000);
+        adr.setKeyStore("234ibfd3840fo");
+        adr.displayProduct();
+        System.out.println("Android KeyStore : "+adr.getKeyStore());
+        System.out.println("");
+    
+        BlackBerry bb = new BlackBerry("Balckberry","RIM","Curve",2000000);
+        bb.setPinBB("BH167GT");
+        bb.displayProduct();
+        System.out.println("PIN : "+bb.getPinBB());
+        System.out.println("");
+    
+         WindowsPhone wp = new WindowsPhone("Nokia","Win8","Lumia",3500000);
+        wp.setWpKeyStore("S415BN");
+        wp.displayProduct();
+        System.out.println("Wp Key Store : "+wp.getWpKeyStore());
+        
+    }
+    
+}
